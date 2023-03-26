df5f00446719a38f1fbc8cfdce3920f2d46f8ac4
Dhana <dhanalakshmilanda@gmail.com>
2023-03-26 15:13:35 +0530

    Create Cube in three js

---
+++ src/threeJsRenderer.js
@@ -0,0 +1,33 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color( 0x505050 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxBufferGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({color: 0x00ff00, roughness: 0.05});
+const cube = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } ));
+const light = new THREE.DirectionalLight( 0xffffff );
+light.position.set( 1, 1, 1 ).normalize();
+scene.add( light );
+
+cube.castShadow = true;
+scene.add(cube);
+
+camera.position.z = 5;
+
+export function animate(props) {
+    for (let i =0 ; i<200 ; i++){
+
+    }
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
+
+// animate();
